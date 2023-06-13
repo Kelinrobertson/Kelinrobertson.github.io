9e7b587a9089616672410a2b01ed3eebaf940c59
Kelin Robertson <kelinrobertson2@gmail.com>
2023-06-13 15:21:40 +0000

    <I think I made the best game>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,7 +47,7 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform(300, 400, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(1300, 400, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
     createPlatform(500, 400, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
     createPlatform(200, 600, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
     createPlatform(700, 179, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
