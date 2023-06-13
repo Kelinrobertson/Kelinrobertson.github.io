360bae6091ebc16b757701da33af9f229ace3647
Kelin Robertson <kelinrobertson2@gmail.com>
2023-06-13 14:43:53 +0000

    index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,28 +47,28 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(300, 400, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(500, 400, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(200, 600, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(700, 179, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(980, 600, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-
+    createCollectable("kennedi", 1000, 100, 6, 0.7); //your example function call
+    createCollectable("grace", 300, 170, 6, 0.7); //your example function call
+    createCollectable("max", 600, 170, 6, 0.7); //your example function call
+    createCollectable("steve", 800, 100, 6, 0.7); //your example function call
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
-
+    createCannon("left", 500, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("right", 700, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("left", 200, 1000); // cannon on left wall, 600px down, shooting once per second
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
